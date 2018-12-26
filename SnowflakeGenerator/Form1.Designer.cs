a3a25f7e006bf20b33ca0d8cb43f8f86570cc623
chaftalie <chaftalie@DESKTOP-IHNFJ9N>
2018-12-26 21:35:04 +0100

    Added basic generating code and added multithreading so you can watch it generate at the moment it looks like an ice flower whats growing on an window

--- SnowflakeGenerator/Form1.Designer.cs
+++ SnowflakeGenerator/Form1.Designer.cs
@@ -28,7 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.pBx_main = new System.Windows.Forms.PictureBox();
+            this.lbl_01 = new System.Windows.Forms.Label();
+            this.tBx = new System.Windows.Forms.TextBox();
+            this.Btn_Generate = new System.Windows.Forms.Button();
+            this.tmr_16ms = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pBx_main)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,23 +48,64 @@
             this.pBx_main.Size = new System.Drawing.Size(843, 524);
             this.pBx_main.TabIndex = 0;
             this.pBx_main.TabStop = false;
+            this.pBx_main.Paint += new System.Windows.Forms.PaintEventHandler(this.pBx_main_Paint);
+            // 
+            // lbl_01
+            // 
+            this.lbl_01.AutoSize = true;
+            this.lbl_01.Location = new System.Drawing.Point(12, 9);
+            this.lbl_01.Name = "lbl_01";
+            this.lbl_01.Size = new System.Drawing.Size(35, 13);
+            this.lbl_01.TabIndex = 1;
+            this.lbl_01.Text = "label1";
+            // 
+            // tBx
+            // 
+            this.tBx.Location = new System.Drawing.Point(53, 6);
+            this.tBx.Name = "tBx";
+            this.tBx.Size = new System.Drawing.Size(100, 20);
+            this.tBx.TabIndex = 2;
+            // 
+            // Btn_Generate
+            // 
+            this.Btn_Generate.Location = new System.Drawing.Point(53, 32);
+            this.Btn_Generate.Name = "Btn_Generate";
+            this.Btn_Generate.Size = new System.Drawing.Size(100, 23);
+            this.Btn_Generate.TabIndex = 3;
+            this.Btn_Generate.Text = "Generate New";
+            this.Btn_Generate.UseVisualStyleBackColor = true;
+            this.Btn_Generate.Click += new System.EventHandler(this.Btn_Generate_Click);
+            // 
+            // tmr_16ms
+            // 
+            this.tmr_16ms.Enabled = true;
+            this.tmr_16ms.Interval = 16;
+            this.tmr_16ms.Tick += new System.EventHandler(this.tmr_16ms_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(838, 519);
+            this.Controls.Add(this.Btn_Generate);
+            this.Controls.Add(this.tBx);
+            this.Controls.Add(this.lbl_01);
             this.Controls.Add(this.pBx_main);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pBx_main)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pBx_main;
+        private System.Windows.Forms.Label lbl_01;
+        private System.Windows.Forms.TextBox tBx;
+        private System.Windows.Forms.Button Btn_Generate;
+        private System.Windows.Forms.Timer tmr_16ms;
     }
 }
 
--- SnowflakeGenerator/Form1.cs
+++ SnowflakeGenerator/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +13,149 @@ namespace SnowflakeGenerator
 {
     public partial class Form1 : Form
     {
+        static Snowflake snowflake;
         public Form1()
         {
             InitializeComponent();
+            lbl_01.BackColor = pBx_main.BackColor;
+
+            snowflake = new Snowflake(pBx_main.Width, 1);
+
+            ThreadStart childref = new ThreadStart(Thread_01);
+            Console.WriteLine("In Main: Creating the Child thread");
+
+            Thread childThread = new Thread(childref);
+            childThread.Start();
+
+            
+        }
+
+        public static void Thread_01()
+        {
+            snowflake.Generate_Snowflake();
+        }
+
+        private void pBx_main_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.TranslateTransform(pBx_main.Width / 2, pBx_main.Height / 2);
+            e.Graphics.ScaleTransform(2, 2);
+            lock (snowflake.particles)
+            {
+                foreach (var particle in snowflake.particles)
+                {
+                    e.Graphics.FillEllipse(Brushes.White, particle.Rectangle);
+                }
+            }
+
+        }
+
+        private void Btn_Generate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tmr_16ms_Tick(object sender, EventArgs e)
+        {
+            pBx_main.Invalidate();
+        }
+    }
+
+    public class Snowflake
+    {
+        private float width;
+        private float radius;
+        public List<Particle> particles;
+        Particle particle;
+        int seed;
+        Random rnd;
+
+        public Snowflake(float width, float radius)
+        {
+
+            this.radius = radius;
+            this.width = width;
+            particles = new List<Particle>();
+            particle = new Particle(width / 2, 0, radius);
+            seed = Guid.NewGuid().GetHashCode();
+            rnd = new Random(seed);
+        }
+
+        public void Generate_Snowflake()
+        {
+            for (int i = 0; i < 2000; i++)
+            {
+                lock (particles)
+                {
+                    MoveParticle();
+                    particles.Add(particle);
+                }
+                particle = new Particle(width / 2, 0, radius);
+            }
+        }
+
+        private void MoveParticle()
+        {
+            while (!Particle_Finished())
+            {
+                particle.X--;
+                particle.Y += rnd.Next(-2000, 2001) / 1000;
+            }
+        }
+
+        private bool Particle_Finished()
+        {
+            bool finished = false;
+            bool intersects = false;
+
+            finished = particle.X < 1;
+
+            foreach (Particle p in particles)
+            {
+                float distance = Distance(p.X, p.Y, particle.X, particle.Y);
+                if (distance < radius * 2)
+                {
+                    intersects = true;
+                    break;
+                }
+            }
+
+            return finished || intersects;
+        }
+
+        private float Distance(float x1, float y1, float x2, float y2)
+        {
+            x1 -= x2;
+            y1 -= y2;
+            x1 = To_Positive(x1);
+            y1 = To_Positive(y1);
+            return (float)Math.Sqrt(Math.Pow(x1, 2) + Math.Pow(y1, 2));
+        }
+
+        private float To_Positive(float num)
+        {
+            if (num < 0) num *= -1;
+            return num;
+        }
+    }
+
+    public class Particle
+    {
+        public Particle(float x, float y, float r)
+        {
+            X = x;
+            Y = y;
+            R = r;
+        }
+
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float R { get; set; }
+        public RectangleF Rectangle
+        {
+            get
+            {
+                return new RectangleF(X, Y, R * 2, R * 2);
+            }
         }
     }
 }
