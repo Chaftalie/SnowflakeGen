a950850d8928f9a6a791330d8570d38445a6cd99
chaftalie <chaftalie@DESKTOP-IHNFJ9N>
2018-12-26 20:19:50 +0100

    Picturebox

--- SnowflakeGenerator/Form1.Designer.cs
+++ SnowflakeGenerator/Form1.Designer.cs
@@ -28,20 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pBx_main = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pBx_main)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pBx_main
+            // 
+            this.pBx_main.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pBx_main.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pBx_main.Location = new System.Drawing.Point(-2, -1);
+            this.pBx_main.Name = "pBx_main";
+            this.pBx_main.Size = new System.Drawing.Size(843, 524);
+            this.pBx_main.TabIndex = 0;
+            this.pBx_main.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(838, 519);
+            this.Controls.Add(this.pBx_main);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pBx_main)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pBx_main;
     }
 }
 
